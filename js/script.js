39ee494d5f32e7c8f707287894daf105b539c4be
Soro Cat <sorousha326@gmail.com>
2019-09-10 10:51:46 -0700

    add changes in js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//business logic for Addressbook
+function AddressBook() {
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+
+//business logic for Contacts
+$(document).ready(function() {
+  $("form#next").submit(function(event) {
+    event.preventDefault()

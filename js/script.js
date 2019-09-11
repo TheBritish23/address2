7fa57b5bb1d0d0bb4d059d1bc16ef28468409b40
Soro Cat <sorousha326@gmail.com>
2019-09-11 10:21:37 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -15,18 +15,50 @@ AddressBook.prototype.assignId = function() {
 }
 
 AddressBook.prototype.findContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        return this.contacts[i];
+  for (var x=0; x < this.contacts.length; x++) {
+    if (this.contacts[x]) {
+      if (this.contacts[x].id == id) {
+        return this.contacts[x];
       }
     }
   };
   return false;
 }
 
+AddressBook.prototype.deleteContact = function(id) {
+  for (var x=0; x < this.contacts.length; x++) {
+    if (this.contacts[x]) {
+      if (this.contacts[x].id == id) {
+        delete this.contacts[x];
+        return true;
+      }
+    }
+  };
+  return false;
+}
 
 //business logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// User Interface Logic 
+var addressBook = new AddressBook();
+
 $(document).ready(function() {
   $("form#next").submit(function(event) {
     event.preventDefault()
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    console.log(addressBook.contacts);
+  })
+ })
